c0ecfb50317f41922e4519c7606f7895df6d042b
farazahmed12 <masterdot707@gmail.com>
2022-01-31 16:27:32 +0500

    Completed App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Hero from "./Hero";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
